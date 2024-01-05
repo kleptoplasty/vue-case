29b081cddc7b8a9b2555ae4e7470fca39e4d4026
oujiarong <2683990221@qq.com>
2024-01-05 09:14:28 +0800

    export excel case

---
+++ case_exportExcel/src/components/TheWelcome.vue
@@ -0,0 +1,31 @@
+<script setup>
+// 安装：https://docs.sheetjs.com/docs/getting-started/installation/nodejs
+import exportJSON from './exportJSON';
+const exportExcel = () => {
+  const title = {
+    BGQ: "报告期",
+    C01_2: "商品购进额本月"
+  }
+  const data = [
+    {
+      BGQ: "2022年2月",
+      C01_2: 123
+    },
+    {
+      BGQ: "2022年3月",
+      C01_2: 456
+    },
+    {
+      BGQ: "2022年4月",
+      C01_2: 789
+    }
+  ]
+  exportJSON.loadExcel(title, data, '统计数据');
+}
+</script>
+
+<template>
+  <div>
+    <button @click="exportExcel()">下载Excel</button>
+  </div>
+</template>
